b2b6e17d52a3d1ad0138379cb3fec8eb065e0866
Blackoutburst <blackoutburst@hotmail.com>
2021-12-04 18:37:14 +0100

    Added basic controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,17 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+
+    private Vector3 velocity;
+
+    void Start() {
+        velocity = new Vector3(0, 0, 0);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        velocity.z = Input.GetAxis("Vertical");
+        velocity.x = Input.GetAxis("Horizontal");
+
+        velocity = velocity.normalized;
+        transform.Translate(velocity * Time.deltaTime * speed);
     }
 }
